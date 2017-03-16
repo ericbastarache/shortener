b9e9bb4cb0f1120894839659ad6942dcc730b329
Eric Bastarache <eric_bastarache@hotmail.com>
2017-03-16 15:14:35 -0400

    Updated base component

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import UrlForm from './components/UrlForm';
+import LinksContainer from './components/LinksContainer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Header title="Simple URL Shortener" />
+        <UrlForm />
+        <LinksContainer />
       </div>
     );
   }
