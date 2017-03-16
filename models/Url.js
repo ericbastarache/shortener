b0ceda2fb5727d9a0822c1159a5c88431e0f4a25
Eric Bastarache <eric_bastarache@hotmail.com>
2017-03-16 15:15:52 -0400

    Updated model

--- models/Url.js
+++ models/Url.js
@@ -4,13 +4,11 @@ mongoose.connect('mongodb://localhost:3317/urls');
 let db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error: '));
-db.once('open', () => {
 
-  let Url = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    link: String,
-    token: String
-  });
+let Url = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  link: String,
+  token: String
 });
 
-module.exports = Url;
+module.exports = mongoose.model('Url', Url);
