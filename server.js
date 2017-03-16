e587f56c3f2c3e9d063fedf0c4d2799f536770c8
ericb <eric_bastarache@hotmail.com>
2017-03-16 04:17:07 -0400

    Updated server entry point

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 let express = require('express');
+let app = express();
 let bodyParser = require('body-parser');
 let path = require('path');
-
-let app = express();
+let routes = require('./routes/routes');
 
 app.use(bodyParser.urlEncoded({extended: true}));
 app.use(bodyParser.json());
@@ -10,6 +10,9 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+
+app.get('/urls', routes.list);
+app.post('/urls/create', routes.create);
 
 app.listen(3100, () => {
   console.log('Server started on port 3100');
