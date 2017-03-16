5244b1d5fb78bd4976e3997ad547a4fbdd62056c
Eric Bastarache <eric_bastarache@hotmail.com>
2017-03-16 15:15:14 -0400

    Updated list item component

---
+++ client/src/components/URL.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class URL extends Component {
+  render () {
+    return (
+      <div>
+        <li style={{borderBottom: '1px dashed #ccc'}}>
+          <a href="#">Some URL</a>
+        </li>
+        <li style={{borderBottom: '1px dashed #ccc'}}>
+          <a href="#">Some URL</a>
+        </li>
+        <li style={{borderBottom: '1px dashed #ccc'}}>
+          <a href="#">Some URL</a>
+        </li>
+      </div>
+    );
+  }
+}
+
+export default URL;
