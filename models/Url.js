c0ea9d63590da2e77541cabe220761304b538c42
Eric Bastarache <eric_bastarache@hotmail.com>
2017-03-17 15:17:16 -0400

    Miscellaneous update

--- models/Url.js
+++ models/Url.js
@@ -1,7 +1,7 @@
 let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 let Url = new Schema({
-  _id: mongoose.Schema.Types.ObjectId,
+  _id: Schema.ObjectId,
   urls: String,
   token: String
 });
