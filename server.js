ce8d543df4dcdeddce005808d52d587f96f6ea9f
Eric Bastarache <eric_bastarache@hotmail.com>
2017-03-17 14:45:44 -0400

    Included mongoose connection

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ let app = express();
 let bodyParser = require('body-parser');
 let path = require('path');
 let routes = require('./routes/routes');
+let mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/urls');
+
+let db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
