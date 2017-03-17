17c3c870490cfdbe6090d367ffdcc62cd7b75173
Eric Bastarache <eric_bastarache@hotmail.com>
2017-03-17 14:45:34 -0400

    Updated model

--- models/Url.js
+++ models/Url.js
@@ -1,10 +1,3 @@
-let mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/urls');
-
-let db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error: '));
-
 let Url = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   link: String,
