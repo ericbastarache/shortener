c4cb9c61b1ccd610823507fe045bb448c7276b5e
Eric Bastarache <eric_bastarache@hotmail.com>
2017-03-18 03:21:45 -0400

    Updated model schema

--- models/Url.js
+++ models/Url.js
@@ -1,7 +1,10 @@
 let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 let Url = new Schema({
-  _id: Schema.ObjectId,
+  _id: {
+    type: Schema.ObjectId,
+    default: new ObjectId()
+  },
   urls: String,
   token: String
 });
