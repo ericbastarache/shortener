295b3ce672fc967d510ada6fbdc90d3ceb182329
Eric Bastarache <eric_bastarache@hotmail.com>
2017-03-18 01:14:55 -0400

    Reverted back to es6

--- controllers/ShortenController.js
+++ controllers/ShortenController.js
@@ -1,14 +1,14 @@
 let Url = require('../models/Url');
 
-exports.index = function (req, res) {
+exports.index =  (req, res) => {
   res.send('Hello world');
 };
 
-exports.all_urls = function (req, res, next) {
+exports.all_urls = (req, res, next) => {
   res.send('NOT YET IMPLEMENTED: List all URLs');
 };
 
-exports.create_url = function (req, res, next) {
+exports.create_url = (req, res, next) => {
   res.send('NOT YET IMPLEMENTED: Create URL');
 };
 
