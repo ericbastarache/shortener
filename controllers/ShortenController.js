e802475b9cd4310570aa09597b3c555eae0be84c
Eric Bastarache <eric_bastarache@hotmail.com>
2017-03-18 03:21:37 -0400

    Small update

--- controllers/ShortenController.js
+++ controllers/ShortenController.js
@@ -5,7 +5,7 @@ exports.index =  (req, res) => {
 };
 
 exports.all_urls = (req, res, next) => {
-  res.send('NOT YET IMPLEMENTED: List all URLs');
+  res.send('NOT YET IMPLEMENTED: Retrieve URLS');
 };
 
 exports.create_url = (req, res, next) => {
