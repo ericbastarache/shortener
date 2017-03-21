69b7c9e6327360e1b1b42aa9d81be09fdb886343
Eric Bastarache <eric_bastarache@hotmail.com>
2017-03-20 23:16:16 -0400

    Updated search bar component

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import { FormControl, FormGroup } from 'react-bootstrap';
 
 class SearchBar extends Component {
+  constructor (props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      value: ''
+    }
+  }
+  handleChange (event) {
+    this.setState({value: event.target.value});
+  }
+
   render () {
     return (
       <div>
         <FormGroup>
-          <FormControl bsClass="form-control" type="text" placeholder={this.props.place}/>
+          <FormControl value={this.state.value} onChange={this.handleChange} bsClass="form-control" type="text" placeholder={this.props.place}/>
         </FormGroup>
       </div>
     );
