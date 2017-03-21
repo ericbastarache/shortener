3956171c4d932aee7d4932982fdec9bf3d9470fe
Eric Bastarache <eric_bastarache@hotmail.com>
2017-03-21 14:55:32 -0400

    Updated entry for server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ let bodyParser = require('body-parser');
 let path = require('path');
 let routes = require('./routes/routes');
 let mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/urls');
 
 let db = mongoose.connection;
@@ -12,9 +13,8 @@ db.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/', routes);
 
