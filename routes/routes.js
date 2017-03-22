563b4c31e84ab07419099d6491bcee5fdb6440bd
Eric Bastarache <eric_bastarache@hotmail.com>
2017-03-21 22:14:58 -0400

    Updated route information

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,6 @@ let router = express.Router();
 let shorten_controller = require('../controllers/ShortenController');
 
 router.get('/:url_id', shorten_controller.get_id);
-router.post('/urls/create', shorten_controller.create_url);
+router.post('/urls/:url(*)', shorten_controller.create_url);
 
 module.exports = router;
